e2815ca23bd440d2719c377d530d37c145d25321
Nick <nclehmann@tutanota.com>
2023-05-11 17:49:19 +0200

    removed more files

--- Batchfiles/gameCloses copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-start /wait D:\Programme\Tutanota\Tutanota Desktop\Tutanota Desktop.exe
-
-start D:\Programme\Tutanota\Tutanota Desktop\Tutanota Desktop.exe
--- Batchfiles/gameCloses.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-start /wait C:\Riot Games\League of Legends\Game\League of Legends.exe
-
-D:\Programme\Tutanota\Tutanota Desktop\Tutanota Desktop.exe
